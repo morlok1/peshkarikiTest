b9e08080b4bd7d6c7b1e72565cd9916ff4c85757
morlok1 <inform.cosmo@yandex.ru>
2017-07-21 02:20:42 +0400

    Form is created

--- index.php
+++ index.php
@@ -2,8 +2,34 @@
 
 require_once 'Candidate.php';
 
-$candidate = new Candidate();
-$candidate->run();
-$candidate->calculateDistance("lolka");
+//$candidate = new Candidate();
+//$candidate->run();
+//$candidate->calculateDistance("lolka");
 
 ?>
+
+
+<html>
+    <head>
+        <title>
+            Тестовое задание
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/fio_form_styles.css">
+
+        <script type="text/javascript" src="js/fio_form_scripts.js"></script>
+    </head>
+    
+    <body>
+
+    <div id="client_form">
+        <form>
+            <span id="fio_text">ФИО: </span><input id="fio_form" placeholder="Трензалоров Доктор Ктоктович">
+            <span id="phone_text">Телефон: </span><input id="phone_form" placeholder="88005553535">
+            <span id="address_text">Адрес: </span><input id="address_form" placeholder="Улица Вязов, дом в собственности">
+            <input type="button" value="Найти" onclick="validateClientForm()" id="client_form_submit_button">
+        </form>
+        <span id="result"></span>
+    </div>
+
+    </body>
+</html>
---
+++ js/fio_form_scripts.js
@@ -0,0 +1,12 @@
+function validateClientForm() {
+    var isValid = true;
+    document.getElementById("result").innerHTML = "lolka";
+
+    if (isValid) {
+        submitClientForm();
+    }
+}
+
+function submitClientForm() {
+    //Здесь будет происходить отправка формы
+}
